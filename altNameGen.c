acb238e43bfcc1d728ea8dd936022643827eb965
Yuvraj Ralh <83678519+yuvi4444@users.noreply.github.com>
2023-11-27 00:46:00 +1100

    Create altNameGen.c

---
+++ altNameGen.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+void altNameGen(char input[], int length){
+    char letter[] = "A";
+    for(int i=0; i<length; i++){
+        // if (input[i] == "A"){
+        //     input[i] = 4;
+        // }
+        // if (strcmp(input[i], letter) == 0){
+        //     input[i] = '4';
+        // }
+        if (input[i] == '4'){
+            input[i] = 'A';
+        }
+        printf("ALT: %c\n", input[i]);
+    }
+    printf("success");
+}
+
+void validate(char input[], int length){
+    if (length > 6){
+        printf("Invalid plate. Try again.\n");
+    }
+    else{
+        printf("Your plate has %d letters.\n", length);
+        altNameGen(input, length);
+    }
+}
+
+int main(){
+    char input[6];
+    printf("Enter your plate number: ");
+    scanf("%s", &input);
+    int length = strlen(input);
+    validate(input, length);
+    return 0;
+}
+
+// alphanum check
+// identify > replace for each alt for all letters
